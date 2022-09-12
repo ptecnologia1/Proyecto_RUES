64f566f5a924eb0de107c4279c9b22e966e613c7
ptecnologia1 <ptecnologia1@procolombia.co>
2022-09-12 14:39:33 -0500

    C
    Creación de proyecto en spring boot

---
+++ RUESSalesforceSearch/src/main/java/com/procolombia/RUESSalesforceSearch/RuesSalesforceBrowserApplication.java
@@ -0,0 +1,13 @@
+package com.procolombia.RUESSalesforceSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuesSalesforceBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuesSalesforceBrowserApplication.class, args);
+	}
+
+}
---
+++ RUESSalesforceSearch/src/test/java/com/procolombia/RUESSalesforceSearch/RuesSalesforceBrowserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.procolombia.RUESSalesforceSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuesSalesforceBrowserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
